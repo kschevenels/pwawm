f48e6ead088472d34954050e4432f201e062c194
kschevenels <110229499+kschevenels@users.noreply.github.com>
2022-11-21 18:12:20 +0100

    Update Table1.R

--- code/Table1.R
+++ code/Table1.R
@@ -25,7 +25,7 @@ patientchars <- PWAWM %>%
                                        education ~ "Education (years)",
                                        stroke_type ~ "Stroke type (ischemia/hemorrhage)",
                                        stroke_laterality ~ "Stroke laterality (left/bilateral)",
-                                       stroke_history ~ "History of stroke (yes/no)",
+                                       stroke_history ~ "History of stroke (no/yes)",
                                        stroke_size ~ "Acute lesion volume (cc)", 
                                        old_lesion_load ~ "Old lesion load (cc)")) %>%
   modify_footnote(all_stat_cols() ~ "Descriptives printed for continuous variables: M (SD); Descriptives printed for categorical variables: n. Note: NIHSS = National Institutes of Health Stroke Scale (maximum score = 42, a higher score corresponds to a more severe stroke).") %>%
@@ -40,14 +40,14 @@ patientchars <- PWAWM %>%
   mutate(N = ifelse(Variable == "__Handedness (right-handed/other)__", paste0(N,"/",32-as.numeric(N)),N)) %>%
   mutate(N = ifelse(Variable == "__Stroke type (ischemia/hemorrhage)__", paste0(N,"/",32-as.numeric(N)),N)) %>%
   mutate(N = ifelse(Variable == "__Stroke laterality (left/bilateral)__", paste0(N,"/",32-as.numeric(N)),N)) %>%
-  mutate(N = ifelse(Variable == "__History of stroke (yes/no)__", paste0(N,"/",32-as.numeric(N)),N)) %>%
+  mutate(N = ifelse(Variable == "__History of stroke (no/yes)__", paste0(32-as.numeric(N),"/",N),N)) %>%
   add_row(.after = 8, Variable = "ACM/ACP/Avert/Abas/multifocal", N = "22/5/1/1/3", Median = "") %>%
   filter(!Variable %in% c("arteria cerebri media","arteria cerebri posterior","arteria vertebralis",
                           "arteria basilaris","anterior choroidal artery","multifocal", "ABas")) %>%
   mutate(Unknown = as.factor(ifelse(Variable=="__Education (years)__",1, NA))) %>%
   dplyr::rename(`N = 32`=2) %>%
   dplyr::rename(`Median (Range)`=3) %>%
-  dplyr::rename(`Missing values (N)` = Unknown) %>%
+  dplyr::rename(`NA` = Unknown) %>%
   mutate(Variable = recode_factor(Variable, "__Education (years)__" = "Education (years)",
                                   "__Age (years)__" = "Age (years)", 
                                   "__stroke_area__" = "Affected circulation area",
@@ -57,7 +57,7 @@ patientchars <- PWAWM %>%
                                   "__Stroke laterality (left/bilateral)__" = "Stroke laterality (left/bilateral)", 
                                   "__Acute lesion volume (cc)__" = "Acute lesion volume (cm3)",
                                   "__Old lesion load (cc)__" = "Old lesion load (cm3)",
-                                  "__History of stroke (yes/no)__" = "History of stroke (yes/no)",
+                                  "__History of stroke (no/yes)__" = "History of stroke (no/yes)",
                                   "__Acute NIHSS total score__" = "Acute NIHSS total score",
                                   "__Days post stroke (acute)__" = "Days post stroke (acute)",
                                   "__Days post stroke (subacute)__" = "Days post stroke (subacute)",
@@ -70,14 +70,18 @@ table_patientchars <- flextable(patientchars) %>%
                       i=1,j=2,
                       value=as_paragraph("N is reported for categorical variables; M (SD) is reported for continuous variables"),
                       ref_symbols = "a") %>%
-  flextable::footnote(part="body",
-                      i=13:15,j=1,
-                      value=as_paragraph("For behavioural language testing"),
+  flextable::footnote(part="header",
+                      i=1,j=4,
+                      value=as_paragraph("N indicates the number of participants for which the corresponding data are missing"),
                       ref_symbols = "b") %>%
   flextable::footnote(part="body",
-                    i=2,j=2,
-                    value=as_paragraph("Note. ACM = arteria cerebri media, ACP = arteria cerebri posterior, Avert = arteria vertebralis, Abas = arteria basilaris, NIHSS = National Institutes of Health Stroke Scale (a higher score corresponds to a more severe stroke)."),
-                    ref_symbols = "") %>%
+                      i=13:15,j=1,
+                      value=as_paragraph("For behavioral language testing"),
+                      ref_symbols = "c") %>%
+  flextable::footnote(part="body",
+                      i=2,j=2,
+                      value=as_paragraph("Note. ACM = arteria cerebri media, ACP = arteria cerebri posterior, Avert = arteria vertebralis, Abas = arteria basilaris, NIHSS = National Institutes of Health Stroke Scale (a higher score corresponds to a more severe stroke)."),
+                      ref_symbols = "") %>%
   align(i=1:15, j=2:4, align = "center", part="body") %>%
   align(j=2:4, align = "center", part="header") %>%
   set_table_properties(layout = "autofit") %>%
